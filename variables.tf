6b4b56a7a27efe259a28c64a73be5a5cdc4fcd0b
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-17 18:59:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "subnet_cidr" { default = "10.0.1.0/24" }
+variable "sg_ingress_port" { default = 22 }
+variable "environment" {default = "dev" }
