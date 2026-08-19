6fb183f1dafb34d955fceafe18486e29be8481a4
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-19 17:37:30 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "vpc_id" { value = module.networking.vpc_id }
-output "subnet_id" { value = module.networking.subnet_id }
-output "sg_id" { value = module.security.sg_id }
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "subnet_id" {
+  value = module.networking.subnet_id
+}
+
+output "sg_id" {
+  value = module.security.sg_id
+}
