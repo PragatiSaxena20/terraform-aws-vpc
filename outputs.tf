67e34d44c1a14b40b7a97a61052fa673f711b5e9
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-22 22:44:11 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
-  description = "ID of the VPC."
+  description = "ID of the VPC"
   value       = module.networking.vpc_id
 }
 
 output "subnet_id" {
-  description = "ID of the subnet."
+  description = "ID of the subnet"
   value       = module.networking.subnet_id
 }
 
 output "sg_id" {
-  description = "ID of the security group."
+  description = "ID of the security group"
   value       = module.security.sg_id
 }
