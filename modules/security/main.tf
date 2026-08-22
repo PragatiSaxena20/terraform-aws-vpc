baf2b1931d762d8b77d54b3ffc92c5aa7c00153f
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-22 22:19:00 +0530

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,33 +1,44 @@
-// Declare input variables
-variable "sg_ingress_port" {}
-variable "environment" {}
-variable "vpc_id" {}
-
-// Security Group resource
-resource "aws_security_group" "my_sg" {
-  vpc_id      = var.vpc_id
-  description = "Allow SSH access"
-
-  ingress {
-    from_port   = var.sg_ingress_port
-    to_port     = var.sg_ingress_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.environment}-sg"
-  }
+variable "sg_ingress_port" {
+  description = "TCP port allowed for inbound traffic."
+  type        = number
 }
 
-// Export SG ID
+variable "environment" {
+  description = "Deployment environment."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the security group will be created."
+  type        = string
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "${var.environment}-sg"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = var.sg_ingress_port
+    to_port     = var.sg_ingress_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-sg"
+  }
+}
+
 output "sg_id" {
-  value = aws_security_group.my_sg.id
+  description = "ID of the security group."
+  value       = aws_security_group.my_sg.id
 }
